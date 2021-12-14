b65a2f0750143020b10936b9e09454c3f5b68fd9
MaruBigCAt <hqtest2020@gmail.com>
2021-12-14 11:22:49 +0800

    New

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // $('input[type=radio]:checked').on('click', function () {
+  //  $('html, body').animate({
+  //      scrollTop: $($(this).attr('href')).offset().top
+  //  }, 500, 'linear');
+
+  //  $('.swiper-slide').css('background-color', '#c0392b');
+
+  //  $( ".swiper-slide" ).click(function() {
+  //    alert( "Handler for .click() called." );
+  //  });
+
+  //  $('input[type=radio]').on('change', function () {
+  //    $('.swiper-slide').css('background-color', '#6b705c');
+  //  });
+  // });
+  
+  // const fruits = [];
+  // fruits[0] = "蘋果 + 5";
+  // fruits[1] = "芭樂 + 0";
+  // fruits[2] = "草莓 + 10";
+  const fruits= ["蘋果 + 5", "芭樂 + 0", "草莓 + 10"];
+  document.getElementById("demo").innerHTML = fruits[1];
+});
